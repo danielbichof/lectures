f62c823ad24864d2a3832952b35632ed1aaada63
DanielBichof <daniel.h.bichof@gmail.com>
2024-02-17 19:49:49 -0300

    operators overloading

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,100 @@
 
 #include <iostream>
 #include <string>
+#include <cmath>
+#include <vector>
 #include "commons.h"
 
-class Entity
+struct Vector2
 {
-private:
-	std::string m_Name;
-	int m_Age;
+	float x, y;
+	Vector2(float x, float y) : x(x), y(y) {}
+	Vector2 Add(const Vector2 &other) const
+	{
+		return *this + other;
+	}
+	Vector2 Multiply(const Vector2 &other) const
+	{
+		return *this * other;
+	}
 
-public:
-	explicit Entity(const std::string &name) : m_Name(name), m_Age(-1) {}
-	Entity(int age) : m_Name("Unknown"), m_Age(age) {}
-	const string &GetName() const { return m_Name; }
+	Vector2 operator+(const Vector2 &other) const
+	{
+		return Vector2(x + other.x, y + other.y);
+	}
+
+	Vector2 operator*(const Vector2 &other) const
+	{
+		return Vector2(x * other.x, y * other.y);
+	}
+	bool operator==(const Vector2 &other) const
+	{
+		return x == other.x || y == other.y;
+	}
+	bool operator!=(const Vector2 &other) const
+	{
+		return !(*this == other);
+	}
 };
 
-void print(const Entity &entity) { std::cout << entity.GetName() << std::endl; }
+struct Integer
+{
+	long int data;
+
+	Integer(int i) : data(i){};
+	int get() const { return data; }
+
+	Integer operator^(const int exponent)
+	{
+		return Integer(std::pow(data, exponent));
+	}
+
+	friend Integer operator^(const Integer &base, const int exponent)
+	{
+		return Integer(static_cast<int>(std::pow(base.data, exponent)));
+	}
+
+	friend Integer operator^(const Integer &base, const Integer exponent)
+	{
+		return Integer(static_cast<int>(std::pow(base.get(), exponent.get())));
+	}
+};
+
+std::ostream &operator<<(std::ostream &stream, const Vector2 &other)
+{
+	stream << other.x << ", " << other.y;
+	return stream;
+}
+
+std::ostream &operator<<(std::ostream &stream, const Integer &obj)
+{
+
+	stream << obj.get();
+	return stream;
+}
 
 int main()
 {
-	std::string name = "Daniel Bichof";
-	auto *entity = new Entity(name);
-	print(*entity);
-	delete entity;
+	Vector2 position(3.0f, 1.0f);
+	Vector2 speed(7.0f, 9.0f);
+	Vector2 powerup(2.0f, 5.f);
 
-	Entity a("Bob");
-	print(a);
+	Vector2 result1 = position + speed * powerup;
+	Vector2 result2 = (position + speed);
+
+	if (result1 != result2)
+	{
+		std::cout << "Not equals: \n\t" << result1 << "\n\t" << result2 << std::endl;
+	}
+
+	Integer res1 = Integer(2) ^ 3;
+	std::cout << "Res: " << res1 << std::endl;
+
+	Integer res2 = 2;
+	res2 = res2 ^ 8;
+	std::cout << "Res: " << res2 << std::endl;
+
+	Integer res3(2);
+	res3 = res3 ^ 10;
+	std::cout << "Res: " << res3 << std::endl;
 }
