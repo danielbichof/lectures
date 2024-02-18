f2a9d149c9176fefa9cd1d73d59b9953d53cc48e
DanielBichof <daniel.h.bichof@gmail.com>
2024-02-18 15:37:20 -0300

    vectors

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,27 @@
 
 #include "commons.h"
+#include <vector>
 
-class String
+struct Vertex
 {
-public:
-	String(const char *string)
-	{
-		m_Size = strlen(string);
-		m_Buffer = new char[m_Size];
-		memcpy(m_Buffer, string, m_Size);
+	float x, y, z;
+	Vertex(float x, float y, float z) : x(x), y(y), z(z) {}
+	Vertex(const Vertex &vertex) : x(vertex.x), y(vertex.y), z(vertex.z)
+	{ /*std::cout << "copied!" << std::endl;*/
 	}
-
-	~String()
-	{
-		delete[] m_Buffer;
-	}
-	String(const String &other) : m_Size(other.m_Size)
-	{
-		m_Buffer = new char[m_Size + 1];
-		memcpy(m_Buffer, other.m_Buffer, m_Size + 1);
-		std::cout << "Copied" << std::endl;
-	}
-	friend std::ostream &operator<<(std::ostream &o, const String &str);
-	char &operator[](const unsigned int index)
-	{
-		return m_Buffer[index];
-	}
-
-private:
-	char *m_Buffer;
-	unsigned int m_Size;
 };
 
-std::ostream &operator<<(std::ostream &o, const String &str)
+std::ostream &operator<<(std::ostream &stream, const Vertex &v)
 {
-	o << str.m_Buffer;
-	return o;
-}
-
-void println(const String &string)
-{
-	std::cout << string << std::endl;
+	stream << v.x << ", " << v.y << ", " << v.z;
+	return stream;
 }
 
 int main()
 {
-	String str = "Daniel";
-	String ss = str;
-	ss[4] = 'l';
-	ss[5] = 'o';
-	println(ss);
-	println(str);
+	std::vector<Vertex> vertices;
+	vertices.reserve(10);
+	vertices.emplace_back(1, 2, 3);
+	vertices.emplace_back(4, 5, 6);
+	vertices.emplace_back(7, 8, 9);
 }
