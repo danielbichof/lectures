95fc6099fcd5d4de7450fc6e476c30ada7bdfc13
DanielBichof <daniel.h.bichof@gmail.com>
2024-02-18 19:37:00 -0300

    linking static lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,10 @@
 
 #include "commons.h"
-#include <vector>
-
-struct Vertex
-{
-	float x, y, z;
-	Vertex(float x, float y, float z) : x(x), y(y), z(z) {}
-	Vertex(const Vertex &vertex) : x(vertex.x), y(vertex.y), z(vertex.z)
-	{ /*std::cout << "copied!" << std::endl;*/
-	}
-};
-
-std::ostream &operator<<(std::ostream &stream, const Vertex &v)
-{
-	stream << v.x << ", " << v.y << ", " << v.z;
-	return stream;
-}
+#include "GLFW/glfw3.h"
 
 int main()
 {
-	std::vector<Vertex> vertices;
-	vertices.reserve(10);
-	vertices.emplace_back(1, 2, 3);
-	vertices.emplace_back(4, 5, 6);
-	vertices.emplace_back(7, 8, 9);
+	int a = glfwInit();
+	std::cout << a << std::endl;
+	std::cin.get();
 }
