18d5062a654b39eaa68db6f8957ea90299d7f912
DanielBichof <daniel.h.bichof@gmail.com>
2024-02-18 13:00:24 -0300

    smart pointers

--- include/commons.h
+++ include/commons.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <memory>
 
 typedef std::string string;
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,34 @@
 
-#include <iostream>
-#include <string>
-#include <cmath>
-#include <vector>
 #include "commons.h"
 
-struct Vector2
+class Entity
 {
-	float x, y;
-	Vector2(float x, float y) : x(x), y(y) {}
-	Vector2 Add(const Vector2 &other) const
+public:
+	int x;
+	Entity() {}
+	Entity(int x) : x(x) {}
+	~Entity()
 	{
-		return *this + other;
-	}
-	Vector2 Multiply(const Vector2 &other) const
-	{
-		return *this * other;
+		std::cout << "Destroyed Entity " << x << std::endl;
 	}
 
-	Vector2 operator+(const Vector2 &other) const
-	{
-		return Vector2(x + other.x, y + other.y);
-	}
-
-	Vector2 operator*(const Vector2 &other) const
-	{
-		return Vector2(x * other.x, y * other.y);
-	}
-	bool operator==(const Vector2 &other) const
-	{
-		return x == other.x || y == other.y;
-	}
-	bool operator!=(const Vector2 &other) const
-	{
-		return !(*this == other);
-	}
+	void print() { std::cout << x << std::endl; }
 };
 
-struct Integer
+void PrintEntity(std::unique_ptr<Entity> &entity)
 {
-	long int data;
-
-	Integer(int i) : data(i){};
-	int get() const { return data; }
-
-	Integer operator^(const int exponent)
-	{
-		return Integer(std::pow(data, exponent));
-	}
-
-	friend Integer operator^(const Integer &base, const int exponent)
-	{
-		return Integer(static_cast<int>(std::pow(base.data, exponent)));
-	}
-
-	friend Integer operator^(const Integer &base, const Integer exponent)
-	{
-		return Integer(static_cast<int>(std::pow(base.get(), exponent.get())));
-	}
-};
-
-std::ostream &operator<<(std::ostream &stream, const Vector2 &other)
-{
-	stream << other.x << ", " << other.y;
-	return stream;
-}
-
-std::ostream &operator<<(std::ostream &stream, const Integer &obj)
-{
-
-	stream << obj.get();
-	return stream;
+	entity->print();
 }
 
 int main()
 {
-	Vector2 position(3.0f, 1.0f);
-	Vector2 speed(7.0f, 9.0f);
-	Vector2 powerup(2.0f, 5.f);
-
-	Vector2 result1 = position + speed * powerup;
-	Vector2 result2 = (position + speed);
-
-	if (result1 != result2)
 	{
-		std::cout << "Not equals: \n\t" << result1 << "\n\t" << result2 << std::endl;
+		std::weak_ptr<Entity> e0;
+		{
+			std::shared_ptr<Entity> entity_shared = std::make_shared<Entity>();
+			e0 = entity_shared;
+			std::unique_ptr<Entity> e_unique = std::make_unique<Entity>(10);
+			PrintEntity(e_unique);
+		}
 	}
-
-	Integer res1 = Integer(2) ^ 3;
-	std::cout << "Res: " << res1 << std::endl;
-
-	Integer res2 = 2;
-	res2 = res2 ^ 8;
-	std::cout << "Res: " << res2 << std::endl;
-
-	Integer res3(2);
-	res3 = res3 ^ 10;
-	std::cout << "Res: " << res3 << std::endl;
 }
