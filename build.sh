83d8627ce05ef3b5264518c9b72859a93b6a5fc8
DanielBichof <daniel.h.bichof@gmail.com>
2024-02-22 20:31:53 -0300

    timers

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ mkdir -p build
 cd build
 rm -rf *
 cmake .. -DCMAKE_BUILD_TYPE=Debug
-ninja
-./main	
+make
+./main
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,47 @@
-#include "GLFW/glfw3.h"
+#include <iostream>
+#include <chrono>
+#include <thread>
 
 #include "commons.h"
 #include "log.h"
+
+struct Timer
+{
+	std::chrono::time_point<std::chrono::system_clock> start;
+	std::chrono::time_point<std::chrono::system_clock> end;
+	std::chrono::duration<float> duration;
+
+	Timer()
+	{
+		start = std::chrono::high_resolution_clock::now();
+	}
+	~Timer()
+	{
+		end = std::chrono::high_resolution_clock::now();
+		duration = end - start;
+		float ms = duration.count() * 1000.0f;
+		std::cout << ms << " ms" << std::endl;
+	}
+};
+
 class log
 {
 public:
 	template <typename T>
-	static void println(T stream)
+	static void info(T stream)
 	{
 		std::cout << stream << std::endl;
 	}
 };
 
+void foo()
+{
+	Timer timer;
+	for (int i = 0; i < 10; i++)
+		log::info("Daniel");
+}
+
 int main()
 {
-	Logger logger;
-	auto name = "daniel";
-	auto a = 2.34;
-	auto x = 90;
-	logger.info(name);
-	logger.info(a);
-	logger.info(x);
+	foo();
 }
